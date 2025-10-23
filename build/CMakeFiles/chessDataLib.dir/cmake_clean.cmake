6a67cfb277aa80e2842f2c061689832f8373fb08
Nidzoki <nikolavidovic01@gmail.com>
2025-10-23 19:32:51 +0200

    Initial commit

---
+++ build/CMakeFiles/chessDataLib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chessDataLib.dir/src/chessDataLib.cpp.obj"
+  "CMakeFiles/chessDataLib.dir/src/chessDataLib.cpp.obj.d"
+  "libchessDataLib.a"
+  "libchessDataLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chessDataLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/chessDataLib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for chessDataLib.
+# This may be replaced when dependencies are built.
